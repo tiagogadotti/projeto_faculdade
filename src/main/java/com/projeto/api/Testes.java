95ed2de67fdf432b994d7eee074d3b57468c4918
tiagogadotti <ti.gadotti@gmail.com>
2023-05-20 18:17:38 -0300

    Faltou comitar as novas classes

---
+++ src/main/java/com/projeto/api/Testes.java
@@ -0,0 +1,18 @@
+package com.projeto.api;
+
+import java.time.LocalDate;
+
+import org.json.JSONException;
+
+public class Testes {
+
+	public static void main(String[] args) {
+		
+		LocalDate date = LocalDate.parse("2014-01-01");
+		System.out.println(date);
+		System.out.println(LocalDate.now().minusYears(18));
+		if (!date.isBefore(LocalDate.now().minusYears(18)))
+			System.out.println("Menor!");
+		
+	}
+}
--- src/main/resources/static/scripts/index.js
+++ src/main/resources/static/scripts/index.js
@@ -28,6 +28,40 @@ $(document).ready(() => {
 				console.error('Error fetching data:', errorThrown);
 			}
 		});
-		
 	})
-});
+	
+	$("#divCriarConta").hide();
+	
+	$("#cliqueAquiParaCadastrar").click( () => {
+		$("#divCriarConta").show();
+	})
+	
+	$("#criarConta").click( (event) =>{
+		event.preventDefault();	
+		const json = {
+			nome: $("#cadastroNome").val(),
+			email: $("#cadastroEmail").val(),
+			senha: $("#cadastroSenha").val(),
+			dataNascimento: $("#cadastroDataNascimento").val()
+		}
+		console.log(json);
+		
+		$.ajax({
+			url: 'http://127.0.0.1:8081/cadastrarUsuario',
+			type: 'POST',
+			dataType: 'json',
+			contentType: 'application/json',
+			data: JSON.stringify(json),
+			success: function(response) {
+				console.log(JSON.stringify(response));
+				window.alert(response.mensagem);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				console.error('Error fetching data:', JSON.stringify(errorThrown));
+				retorno = JSON.parse(jqXHR.responseText);
+				window.alert(retorno.mensagem);
+			}
+		});
+	})
+	
+})
