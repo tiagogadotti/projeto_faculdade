602a1bf778c09791aa11d7901cd7ad7d744d7962
tiagogadotti <ti.gadotti@gmail.com>
2023-06-05 13:00:47 -0300

    Adicionnado json do Postman

--- src/main/java/com/projeto/api/CategoriaController.java
+++ src/main/java/com/projeto/api/CategoriaController.java
@@ -1,16 +1,9 @@
 package com.projeto.api;
 
-import java.sql.*;
-import java.util.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.projeto.*;
 import com.projeto.dto.CategoriaDTO;
-import com.projeto.model.Categoria;
 import com.projeto.service.CategoriaService;
 
 @RestController
@@ -26,15 +19,15 @@ public class CategoriaController {
 	public ResponseEntity<?> saveCategoria(@RequestBody CategoriaDTO categoriaDto) {
 		try {
 			categoriaService.saveCategoria(categoriaDto);
-			return new ResponseEntity(categoriaDto, HttpStatus.CREATED);
+			return new ResponseEntity<CategoriaDTO>(categoriaDto, HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity("ERRO AO CRIAR CATEGORIA: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>("ERRO AO CRIAR CATEGORIA: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
 	@GetMapping("/api/listCategoria")
 	public ResponseEntity<?> listarCategorias() {
-		return new ResponseEntity(categoriaService.listarCategorias(), HttpStatus.OK);
+		return new ResponseEntity<>(categoriaService.listarCategorias(), HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/api/deleteCategoriasById")
